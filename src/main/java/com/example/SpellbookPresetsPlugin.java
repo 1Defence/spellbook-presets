c20ddeaf297c84701b3f4a3a1a0ef32483c59645
1Defence <63735241+1Defence@users.noreply.github.com>
2025-12-16 05:14:21 -0500

    update copyright

--- src/main/java/com/example/SpellbookPresetsPlugin.java
+++ src/main/java/com/example/SpellbookPresetsPlugin.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2024, Adam <Adam@sigterm.info>
- * Copyright (c) 2025, 1Defence
+ * Copyright (c) 2025, Jamal <http://github.com/1Defence>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
